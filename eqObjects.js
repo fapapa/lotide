e358e160511ca07d2b59966eb093b46f45c26ae3
Fabio Papa <fabtheman@gmail.com>
2019-09-18 16:30:19 -0700

    Implement eqObjects

---
+++ eqObjects.js
@@ -0,0 +1,65 @@
+const assertEqual = function(actual, expected) {
+  let msg;
+
+  if (actual === expected) {
+    msg = `👍👍👍Assertion Passed: ${actual} === ${expected}👍👍👍`;
+  } else {
+    msg = `☢☢☢Assertion Failed: ${actual} !== ${expected}☢☢☢`;
+  }
+
+  console.log(msg);
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) return false;
+
+  for (let key in object1) {
+    if (Array.isArray(object1[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (typeof object1[key] === 'object') {
+      if (!eqObjects(object1[key], object2[key])) {
+        return false;
+      }
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
+
+// Primitives
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false);
+
+// Arrays
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+eqObjects(cd, cd2); // => false
+assertEqual(eqObjects(cd, cd2), false);
+
+// Objects
+const ef = { e: "1", f: { g: 2, h: 3 } };
+const fe = { f: { g: 2, h: 3 }, e: "1" };
+assertEqual(eqObjects(ef, fe), true);
