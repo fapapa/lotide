3b1a35b989ce6d9153467444bb7c3b0fbe196518
Fabio Papa <fabtheman@gmail.com>
2019-09-17 21:08:47 -0700

    Implement the without function

---
+++ without.js
@@ -0,0 +1,31 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  let msg;
+
+  if (eqArrays(actual, expected)) {
+    msg = `👍👍👍Assertion Passed: [${actual}] === [${expected}]👍👍👍`;
+  } else {
+    msg = `☢☢☢Assertion Failed: [${actual}] !== [${expected}]☢☢☢`;
+  }
+
+  console.log(msg);
+};
+
+const without = function(source, itemsToRemove) {
+  return source.reduce((result, item) => {
+    if (itemsToRemove.indexOf(item) < 0) result.push(item);
+    return result;
+  }, []);
+};
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
