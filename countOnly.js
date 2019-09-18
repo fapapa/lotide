abafcdbe8bcb5ddfafb0505ccc9f5f0fe4cacbcc
Fabio Papa <fabtheman@gmail.com>
2019-09-18 13:00:23 -0700

    Implement the countOnly function

---
+++ countOnly.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+  let msg;
+
+  if (actual === expected) {
+    msg = `👍👍👍Assertion Passed: ${actual} === ${expected}👍👍👍`;
+  } else {
+    msg = `☢☢☢Assertion Failed: ${actual} !== ${expected}☢☢☢`;
+  }
+
+  console.log(msg);
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  return allItems.reduce((itemsCount, item) => {
+    if (itemsToCount[item]) {
+      if (itemsCount[item]) {
+        itemsCount[item]++;
+      } else {
+        itemsCount[item] = 1;
+      }
+    }
+    return itemsCount;
+  }, {});
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
