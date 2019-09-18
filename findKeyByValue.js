4e87b7950bedb1c5e26ec4caa2c84a1aea4ae2d8
Fabio Papa <fabtheman@gmail.com>
2019-09-18 16:30:02 -0700

    Implement findKeybyvalue

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  let msg;
+
+  if (actual === expected) {
+    msg = `👍👍👍Assertion Passed: ${actual} === ${expected}👍👍👍`;
+  } else {
+    msg = `☢☢☢Assertion Failed: ${actual} !== ${expected}☢☢☢`;
+  }
+
+  console.log(msg);
+};
+
+const findKeyByValue = function(obj, val) {
+  for (const property in obj) {
+    if (obj[property] === val) return property;
+  }
+
+  return undefined;
+};
+
+let test = {
+  firstName: "Fabio",
+  lastName: "Papa"
+};
+
+assertEqual(findKeyByValue(test, "Fabio"), "");
