44a03d7760a4f3d43cd94a75dbc503918ebdb936
Fabio Papa <fabtheman@gmail.com>
2019-09-17 19:56:46 -0700

    Implement middle

---
+++ middle.js
@@ -0,0 +1,42 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  let msg;
+
+  if (eqArrays(actual, expected)) {
+    msg = `👍👍👍Assertion Passed: [${actual}] === [${expected}]👍👍👍`;
+  } else {
+    msg = `☢☢☢Assertion Failed: [${actual}] !== [${expected}]☢☢☢`;
+  }
+
+  console.log(msg);
+};
+
+const middle = function(arr) {
+  let len = arr.length;
+
+  if (len < 3) return [];
+
+  if (len % 2 === 1) {
+    return [ arr[Math.floor(len / 2)] ];
+  } else {
+    return arr.slice((Math.floor(len / 2) - 1), (Math.floor(len / 2) + 1));
+  }
+};
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
