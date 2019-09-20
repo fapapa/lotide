d1c3379d63bcee1f0471be2c486f2410dd690996
Fabio Papa <fabtheman@gmail.com>
2019-09-20 15:52:39 -0700

    Add test cases to eqArrays for recursive cases

--- eqArrays.js
+++ eqArrays.js
@@ -25,3 +25,8 @@ assertEqual(eqArrays([], []), true);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
 assertEqual(eqArrays(["hi"], ["hi"]), true);
 assertEqual(eqArrays([], [1]), false);
+
+eqArrays([[2, 3], [4]], [[2, 3], [4]]); // => true
+
+eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]); // => false
+eqArrays([[2, 3], [4]], [[2, 3], 4]); // => false
