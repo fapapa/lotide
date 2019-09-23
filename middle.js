fa7def5d8721bac511b85c7dba05e2f33f67d44b
Fabio Papa <fabtheman@gmail.com>
2019-09-23 15:07:10 -0700

    Change the tests for middle into Mocha/Chai tests

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  let msg;
-
-  if (eqArrays(actual, expected)) {
-    msg = `👍👍👍Assertion Passed: [${actual}] === [${expected}]👍👍👍`;
-  } else {
-    msg = `☢☢☢Assertion Failed: [${actual}] !== [${expected}]☢☢☢`;
-  }
-
-  console.log(msg);
-};
-
 const middle = function(arr) {
   let len = arr.length;
 
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,24 @@
-const assertArraysEqual = require('../assertArraysEqual.js');
+const assert = require('chai').assert;
 const middle = require('../middle.js');
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+describe('#middle', () => {
+  it('returns the middle item of an odd-length array', () => {
+    const odd = [1, 2, 3];
+    assert.deepEqual(middle(odd), [2]);
+  });
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+  it('returns the middle two elements of an even-length array', () => {
+    const even = [1, 2, 3, 4];
+    assert.deepEqual(middle(even), [2, 3]);
+  });
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  it('returns an empty array when a single-element array is passed in', () => {
+    const one = [1];
+    assert.deepEqual(middle(one), []);
+  });
+
+  it('returns an empty array when a two-element array is passed in', () => {
+    const two = [2];
+    assert.deepEqual(middle(two), []);
+  });
+});
