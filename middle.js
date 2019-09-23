d5b7ad49036f7ae2cf40540dd8ae7501a090230b
Fabio Papa <fabtheman@gmail.com>
2019-09-23 13:00:13 -0700

    Move tests from the middle function into its own module
    Pair programmed with @adamleseur

--- middle.js
+++ middle.js
@@ -32,11 +32,4 @@ const middle = function(arr) {
   }
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
